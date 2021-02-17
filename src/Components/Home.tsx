fb5374cd629b37252de6681af3dc3de04de9148a
Marcio Medeiros <marciomed07@gmail.com>
2021-02-17 20:18:57 -0300

    Updating chart when currency changes and adding color to the bars

--- src/Components/Home.tsx
+++ src/Components/Home.tsx
@@ -6,10 +6,19 @@ import Miners from '../Interfaces/Miners';
 const currencies = Object.freeze({"usd":0, "brl":1, "eth":2});
 let currency = currencies.eth;
 
+const chartColor = [
+    'rgba(255, 99, 132, 1)',
+    'rgba(54, 162, 235, 1)',
+    'rgba(255, 206, 86, 1)',
+    'rgba(75, 192, 192, 1)',
+    'rgba(153, 102, 255, 1)',
+    'rgba(255, 159, 64, 1)'
+]
+
 export default function Home(){
     const [miners, setMiners] = useState<Miners>({});
     useEffect(() => {
-        (async ()=> {setMiners(await getProfit());})();
+        (async ()=> {await getProfit(setMiners, setChart);})();
       }, []);
     const [curChart, setChart] = useState<JSX.Element>(noDataChart)
     return (
@@ -19,7 +28,7 @@ export default function Home(){
             </h3>
             <span>
                 <button onClick={()=>setChart(propChart(miners))}>Prop</button>
-                <button onClick={()=>setChart(dindinChart(miners))}>Dindin</button>
+                <button onClick={()=>setChart(dindinChart(miners, setChart))}>Dindin</button>
                 <button onClick={()=>setChart(hashrateChart(miners))}>Hashrate</button>
             </span>
             <div>
@@ -29,14 +38,15 @@ export default function Home(){
     )
 }
 
-async function getProfit():Promise<Miners>{
+async function getProfit(setMiners:Function, setChart:Function):Promise<Miners>{
     const headers= {      
     }
     return fetch(process.env.REACT_APP_API_URL + 'miners/profitDB', headers)
     .then(response => response.json())
     .then<Miners>(data => {
         const treatedData:Miners = formatData(data)
-        
+        setMiners(treatedData);
+        setChart(propChart(treatedData))
         return treatedData
     })
 }
@@ -55,6 +65,7 @@ function propChart(miners:Miners){
         labels: Object.keys(miners).sort((a,b)=>miners[b][prop]-miners[a][prop]),
         datasets: [
             {
+                backgroundColor:chartColor,
                 label: prop,
                 data: Object.values(miners).map(m=>m[prop]).sort((a,b)=>b-a)
             }
@@ -68,7 +79,7 @@ function propChart(miners:Miners){
     </>)
 }
 
-function changeCurrency(){
+function changeCurrency(setChart:Function, miners:Miners){
     switch (currency){
         case currencies.usd:
             currency = currencies.brl;
@@ -80,11 +91,10 @@ function changeCurrency(){
                 currency = currencies.usd;
             break;
     }
-    //setChart(dindinChart(miners)) o que eu queria fazer, mas naturalemnte nao da
+    setChart(dindinChart(miners,setChart))
 }
 
-function dindinChart(miners:Miners){
-    
+function dindinChart(miners:Miners, setChart:Function){    
     let currString = "";
     const prop = ["Dindin", "DindinBRL", "ETH"] as const;
 
@@ -105,17 +115,21 @@ function dindinChart(miners:Miners){
         labels: Object.keys(miners).sort((a,b)=>miners[b][prop[currency]]-miners[a][prop[currency]]),
         datasets: [
             {
+                backgroundColor:chartColor,
                 label: prop[currency] + currString,
                 data: Object.values(miners).map(m=>m[prop[currency]]).sort((a,b)=>b-a)
             }
         ]
     }
-    return(<>
-        <button type="button" onClick={changeCurrency}>Mudar Moeda</button> 
-        <Bar
-            data={data}
-            options={{ maintainAspectRatio: false }}
-        />
+    return(
+    <>
+        <button type="button" onClick={()=>{changeCurrency(setChart, miners);}}>Mudar Moeda</button> 
+        <div>
+            <Bar
+                data={data}
+                options={{ maintainAspectRatio: false }}
+            />
+        </div>
     </>)
 }
 
@@ -125,15 +139,16 @@ function hashrateChart(miners:Miners){
         labels: Object.keys(miners).sort((a,b)=>miners[b][prop]-miners[a][prop]),
         datasets: [
             {
+                backgroundColor:chartColor,
                 label: prop + " (Mh/s)",
                 data: Object.values(miners).map(m=>m[prop]).sort((a,b)=>b-a).map(a => {return a/1e6})
             }
         ]
     }
-    return(<>
+    return(
         <Bar
             data={data}
             options={{ maintainAspectRatio: false }}
         />
-    </>)
+    )
 }
